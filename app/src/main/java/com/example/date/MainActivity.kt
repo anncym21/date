b4f4f14f93a2eee52066d6fb9cdaf48e1318db3c
student <cymanowska.anna@wp.pl>
2022-04-01 15:06:29 +0200

    Data

--- app/src/main/java/com/example/date/MainActivity.kt
+++ app/src/main/java/com/example/date/MainActivity.kt
@@ -1,11 +1,18 @@
-package com.example.date
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.date.R
+import java.text.SimpleDateFormat
+import java.util.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "KotlinApp"
+        val textView: TextView = findViewById(R.id.textView)
+        val calendar: Calendar = Calendar.getInstance()
+        val simpleDateFormat = SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a")
+        val dateTime = simpleDateFormat.format(calendar.time)
+        textView.text = dateTime
     }
 }
